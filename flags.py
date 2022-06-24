db1773c669ae990c31a7bae23ad6de7499413856
Adm <sona@pm.me>
2022-06-24 14:36:39 +0900

    mod ckpt file name

--- flags.py
+++ flags.py
@@ -30,15 +30,15 @@ flags.DEFINE_boolean('save', True, 'wether save the model after training')
 flags.DEFINE_boolean('plot', True, 'wether plot prediction of the model.')
 flags.DEFINE_integer("epoch", 100, "")
 
-ckpt_file_name = 'EP_{epoch}, L_{loss:.3f}, P_{Precision:.3f}, R_{Recall:.3f}, J_{JSC:.3f}, ' \
-                 'vL_{val_loss:.3f}, vP_{val_Precision:.3f}, vR_{val_Recall:.3f}, vJ_{val_JSC:.3f}'\
+ckpt_file_name = 'EP_{epoch}, L_{loss:.0f}, P_{Precision:.3f}, R_{Recall:.3f}, F2_{F2_Score:.3f}, D_{DSC:.3f}, J_{JSC:.3f} ' \
+                 'vL_{val_loss:.0f}, vP_{val_Precision:.3f}, vR_{val_Recall:.3f}, vF2_{val_F2_Score:.3f}, vD_{val_DSC:.3f}, vJ_{val_JSC:.3f}'\
                  '.h5'
 flags.DEFINE_string('ckpt_file_name', ckpt_file_name, 'checkpoint file name')
 
 ########################################################################################################################
 """ Dataset Setting """
 ########################################################################################################################
-flags.DEFINE_integer("bsz", 38, "Batch size")
+flags.DEFINE_integer("bsz", 19, "Batch size")
 flags.DEFINE_integer("repeat", 1, "Batch size")
 flags.DEFINE_float("valid_split", 0.1, "Batch size")
 
